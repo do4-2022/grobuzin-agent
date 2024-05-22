4acec5bed81a1282d14018ed0ba73ffaea623187
Nils Ponsard <nilsponsard@gmail.com>
2024-05-22 13:54:55 +0200

    feat: update timestamp in database

--- image-builder/database.go
+++ image-builder/database.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -12,6 +13,7 @@ func StartDBConnection(connectionStr string) (db *sql.DB, err error) {
 }
 
 func SetFunctionReady(db *sql.DB, id string) (err error) {
-	_, err = db.Exec("UPDATE functions SET built = true WHERE id = $1", id)
+	timestamp := time.Now().Unix()
+	_, err = db.Exec("UPDATE functions SET built = true, build_timestamp = $2 WHERE id = $1", id, timestamp)
 	return
 }
